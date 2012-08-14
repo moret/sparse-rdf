6702450b79e7e863c155cd51fcc7de0045a0d9de
Danilo Moret <github@moret.pro.br>
2012-08-14 15:49:52 -0300

    test home content

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,3 +4,8 @@ from tornado.httpclient import HTTPClient
 def test_exists():
     response = HTTPClient().fetch('http://lvh.me:9999/')
     assert 200 == response.code
+
+
+def test_home_has_generic_message():
+    response = HTTPClient().fetch('http://lvh.me:9999/')
+    assert 'for the future' in response.body
