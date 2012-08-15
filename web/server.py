70cb78a8e263d51a7e5fcf34e91553d6a621b453
Danilo Moret <github@moret.pro.br>
2012-08-15 11:05:31 -0300

    prototype api for node query

--- web/server.py
+++ web/server.py
@@ -2,8 +2,11 @@
 from __future__ import absolute_import
 
 import os
+import json
+
 import daemon
 import lockfile
+
 import tornado.ioloop
 import tornado.web
 import tornado.auth
@@ -11,14 +14,29 @@ import tornado.auth
 from conf.settings import confs
 
 
-class HomeHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('for the future')
+class NodeQueryHandler(tornado.web.RequestHandler):
+    def get(self, node_param):
+        from app.matrix_searcher import matrix_searcher
+        from app.persistance import db
+
+        node_index = db.search_node(node_param)
+
+        result = {
+                'query': node_param,
+                'selected_node': None,
+                'query_result': None
+        }
+
+        if node_index != None:
+            result['selected_node'] = db.get_node(node_index)
+            result['query_result'] = matrix_searcher.node_query(node_index)
+
+        self.finish(json.dumps(result))
 
 
 def start():
     application = tornado.web.Application([
-            (r'/', HomeHandler),
+            (r'/node-query/(.*)', NodeQueryHandler),
         ], **{
             'static_path': os.path.join('static'),
             'template_path': os.path.join('templates'),
